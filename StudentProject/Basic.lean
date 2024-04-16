f44e70b5ebad7efe7f9a9e60b44e2f4d075da27b
Taylor Belcher <taylorgbelcher@gmail.com>
2024-04-16 11:18:17 -0400

    trying from computer

--- StudentProject/Basic.lean
+++ StudentProject/Basic.lean
@@ -10,6 +10,34 @@
 -- and so abc ≥ ab + bc + ca ≥ a + b + c, contradiction.
 
 import Mathlib.Data.Real.Basic
-import Mathlib.Tactic
-example {a b c : ℤ } (ha: a > 0) (hb : b > 0) ( hc : c > 0) (h : a + b + c ≥ a * b * c) : ¬ (a^2 + b^2 + c^2 < a * b * c) := by
-intro h
+import Mathlib.Tactic.Linarith
+example {a b c : ℝ } (ha: a > 0) (hb : b > 0) ( hc : c > 0) (h : a + b + c ≥ a * b * c) : ¬ (a ^ 2 + b ^ 2 + c ^ 2 < a * b * c) := by
+-- Suppose by way of contradiction that a^2 + b^2 + c^2 < abc
+intro H
+
+-- abc > a^2
+have ha2 : a * b * c > a ^ 2 := by
+  calc
+    a * b * c > a ^ 2 + b ^ 2 + c ^ 2 := by linarith [H]
+    _ > a ^ 2 + 0 ^ 2 + 0 ^ 2 := by rel [hb,hc]
+    _ = a ^ 2 := by ring
+
+-- so a < bc
+
+-- abc > b^2
+have hb2 : a * b * c > b ^ 2 := by
+  calc
+    a * b * c > a ^ 2 + b ^ 2 + c ^ 2 := by linarith [H]
+    _ > 0 ^ 2 + b ^ 2 + 0 ^ 2 := by rel [ha,hc]
+    _ = b ^ 2 := by ring
+
+-- so b < ac
+
+-- abc > c^2
+have hc2 : a * b * c > c ^ 2 := by
+  calc
+    a * b * c > a ^ 2 + b ^ 2 + c ^ 2 := by linarith [H]
+    _ > 0 ^ 2 + 0 ^ 2 + c ^ 2 := by rel [ha,hc]
+    _ = c ^ 2 := by ring
+
+-- so c < ab
