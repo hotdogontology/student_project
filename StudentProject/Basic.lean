aab47d2add587abcc49fd6e162a61549d5251479
Taylor Belcher <taylorgbelcher@gmail.com>
2024-04-16 10:34:46 -0400

    problem statement complete

--- StudentProject/Basic.lean
+++ StudentProject/Basic.lean
@@ -4,9 +4,12 @@
 
 -- Solution
 -- We may assume that a,b,c > 0.
--- Suppose by way of contradiction that a^2 + b^2 +c^2; then
--- abc > a^2
+-- Suppose by way of contradiction that a^2 + b^2 + c^2 < abc; then
+-- abc > a^2 and so a < bc, and likewise b < ca, c < ab. Then
+-- abc ≥ a^2 + b^2 + c^2 ≥ ab + bc + ca by AM-GM
+-- and so abc ≥ ab + bc + ca ≥ a + b + c, contradiction.
+
 import Mathlib.Data.Real.Basic
+import Mathlib.Tactic
 example {a b c : ℤ } (ha: a > 0) (hb : b > 0) ( hc : c > 0) (h : a + b + c ≥ a * b * c) : ¬ (a^2 + b^2 + c^2 < a * b * c) := by
 intro h
-have h' : a * b * c ≥ a^2
