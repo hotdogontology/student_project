5e12c085cbdceba795860f6d425ef2bc77b9b9cf
Taylor Belcher <taylorgbelcher@gmail.com>
2024-05-02 11:18:14 -0400

    trying to skip amgm fo rnow

--- StudentProject/Basic.lean
+++ StudentProject/Basic.lean
@@ -92,18 +92,27 @@ have h1 : a + b + c < a * b + b * c + a * c := by
     _ < (a * c + b * c) + a * b := by rel [hc3]
     _ = a * b + b * c + a * c := by ring
 
-sorry
+
 -- plan: write lemma that takes in two real numbers and applies the AM-GM for 2 numbers to it
 --https://github.com/leanprover-community/mathlib4/blob/03b471425ef6894a1385678605489d7ef289754b/Mathlib/Analysis/MeanInequalities.lean#L201-L205
 -- def am_gm2 (x : ℝ) (y : ℝ) := add_rpow_le_rpow_add (Real.toNNreal x) (Real.toNNreal y)
-
+-- justify that x ^ (1/2) ^2 = x : https://leanprover-community.github.io/mathlib4_docs/Mathlib/Analysis/SpecialFunctions/Pow/Real.html#Real.pow_rpow_inv_natCast
 -- apply AM-GM to a,b
+have am_gm_ab : (1/2) * a + (1/2) * b ≥ (a * b) ^ (1/2) := by sorry
 -- a^2 + b^2 ≥ 2ab
+have hab : a ^ 2 + b ^ 2 ≥ 2 * a * b := by
+  calc
+    a ^ 2 + b ^ 2 = a ^ 2 + 2 * a * b + b ^ 2 - 2 * a * b := by ring
+    _ = 4 * (a ^ 2 + 2 * a * b + b ^ 2 - 2 * a * b) / 4 := by ring
+    _
+
 
 -- apply AM-GM to b,c
+have am_gm_bc : (1/2) * b + (1/2) * c ≥ (b * c) ^ (1/2) := by sorry
 -- b^2 + c^2 ≥ 2bc
 
 -- apply AM-GM to a,c
+have am_gm_ac : (1/2) * a + (1/2) * c ≥ (a * c) ^ (1/2) := by sorry
 -- a^2 + c^2 ≥ 2ac
 
 -- add three cases of AM-GM and divide by 2
