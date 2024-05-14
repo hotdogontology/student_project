77cbeb1784a51329af4f4146f85f91d97d7e9774
yuanni-e <109918748+yuanni-e@users.noreply.github.com>
2024-05-14 10:12:57 -0400

    thank you martin

--- StudentProject/Basic.lean
+++ StudentProject/Basic.lean
@@ -13,13 +13,13 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Tactic.Linarith
 import Mathlib.Tactic.FieldSimp
 import Mathlib.Algebra.Order.Field.Basic
-import Mathlib.Analysis.MeanInequalitiesPow
+import Mathlib.Analysis.MeanInequalities
+import Mathlib.Data.Real.NNReal
+import Mathlib.Analysis.SpecialFunctions.Pow.Real
+import Mathlib.Algebra.Order.Ring.Defs
 
---might need Real namespace instead?
-namespace NNReal
+namespace Real
 
-
---do we need an open statement here?
 open Classical
 
 example {a b c : ℝ } (ha: a > 0) (hb : b > 0) ( hc : c > 0) (h : a + b + c ≥ a * b * c) : a ^ 2 + b ^ 2 + c ^ 2 ≥  a * b * c := by
@@ -27,10 +27,9 @@ example {a b c : ℝ } (ha: a > 0) (hb : b > 0) ( hc : c > 0) (h : a + b + c ≥
 by_contra! H
 
 -- abc > a^2
-have ha2 : a * b * c > a ^ 2 := by
-  calc
-    a * b * c > a ^ 2 + b ^ 2 + c ^ 2 := by linarith [H]
-    _ > a ^ 2 + 0 ^ 2 + 0 ^ 2 := by rel [hb,hc]
+have ha2 :=
+  calc a * b * c > a ^ 2 + b ^ 2 + c ^ 2 := by linarith [H]
+    _ > a ^ 2 + 0 ^ 2 + 0 ^ 2 := by rel [hb, hc]
     _ = a ^ 2 := by ring
 
 -- so a < bc
@@ -47,10 +46,9 @@ have ha3 : a < b * c := by
     _ = b * c := by ring
 
 -- abc > b^2
-have hb2 : a * b * c > b ^ 2 := by
-  calc
-    a * b * c > a ^ 2 + b ^ 2 + c ^ 2 := by linarith [H]
-    _ > 0 ^ 2 + b ^ 2 + 0 ^ 2 := by rel [ha,hc]
+have hb2 :=
+  calc a * b * c > a ^ 2 + b ^ 2 + c ^ 2 := by linarith [H]
+    _ > 0 ^ 2 + b ^ 2 + 0 ^ 2 := by rel [ha, hc]
     _ = b ^ 2 := by ring
 
 -- so b < ac
@@ -67,10 +65,9 @@ have hb3 : b < a * c := by
     _ = a * c := by ring
 
 -- abc > c^2
-have hc2 : a * b * c > c ^ 2 := by
-  calc
-    a * b * c > a ^ 2 + b ^ 2 + c ^ 2 := by linarith [H]
-    _ > 0 ^ 2 + 0 ^ 2 + c ^ 2 := by rel [ha,hb]
+have hc2 :=
+  calc a * b * c > a ^ 2 + b ^ 2 + c ^ 2 := by linarith [H]
+    _ > 0 ^ 2 + 0 ^ 2 + c ^ 2 := by rel [ha, hb]
     _ = c ^ 2 := by ring
 
 -- so c < ab
@@ -85,6 +82,8 @@ have hc3 : c < a * b := by
     _ = a * b * 1 := by field_simp
     _ = a * b := by ring
 
+-- abc ≥ a^2 + b^2 + c^2 ≥ ab + bc + ca by AM-GM
+
 --a + b + c < ab + bc + ca
 have h1 : a + b + c < a * b + b * c + a * c := by
   calc
@@ -96,50 +95,38 @@ have h1 : a + b + c < a * b + b * c + a * c := by
     _ < (a * c + b * c) + a * b := by rel [hc3]
     _ = a * b + b * c + a * c := by ring
 
+--(a + b)/2 ≥ (ab).sqrt using amgm
+--might not need this step now
+--thank you martin from zulip
+have ann : 0 ≤ a := le_of_lt ha
+have bnn : 0 ≤ b := le_of_lt hb
+have abnn : 0 ≤ a * b := Left.mul_nonneg ann bnn
 
--- plan: write lemma that takes in two real numbers and applies the AM-GM for 2 numbers to it
---https://github.com/leanprover-community/mathlib4/blob/03b471425ef6894a1385678605489d7ef289754b/Mathlib/Analysis/MeanInequalities.lean#L201-L205
--- def am_gm2 (x : ℝ) (y : ℝ) := add_rpow_le_rpow_add (Real.toNNreal x) (Real.toNNreal y)
--- justify that x ^ (1/2) ^ 2 = x : https://leanprover-community.github.io/mathlib4_docs/Mathlib/Analysis/SpecialFunctions/Pow/Real.html#Real.rpow_inv_natCast_pow
--- connecting sqrt to fractional power also an issue
--- could probably streamline the proof if we had a lemma that took into two real numbers xy and returned xy_am_gm instead of repeating three times like we do
+have hab : (a + b) / 2 ≥ (a * b).sqrt := by
+  have amgm := two_mul_le_add_sq a.sqrt b.sqrt
+  rw [Real.sqrt_mul']
+  rw [sq_sqrt ann, sq_sqrt bnn] at amgm
+  linarith
+  exact bnn
 
--- apply AM-GM to a,b
-have ab_am_gm : (a + b) / 2 ≥ (a * b) ^ (1/2) := by sorry
+-- (b + c)/2 ≥ (bc).sqrt using amgm
+have hbc : (b + c) / 2 ≥ (b * c) ^ (1/2) := by
+  sorry
 
--- a^2 + b^2 ≥ 2ab
-have hab : a ^ 2 + b ^ 2 ≥ 2 * a * b := by
-  calc
-    a ^ 2 + b ^ 2 = (a + b) ^ 2 - 2 * a * b := by ring
-    _ = 4 * ((a + b) / 2) ^ 2 - 2 * a * b := by ring
-    _ ≥ 4 * ((a * b) ^ (1/2)) ^ 2 - 2 * a * b := by rel [ab_am_gm]
-    _ = 4 * a * b - 2 * a * b := by sorry
-    _ = 2 * a * b := by ring
+-- (a + c)/2 ≥ (ac).sqrt using amgm
+have hac : (a + c) / 2 ≥ (a * c) ^ (1/2) := by
+  sorry
 
+-- (a^2 + 2ab + b^2) ≥ ab
+have hab2 : a ^ 2 + b ^ 2 ≥ 2 * a * b := two_mul_le_add_sq a b
 
--- apply AM-GM to b,c
-have bc_am_gm : (1/2) * b + (1/2) * c ≥ (b * c) ^ (1/2) := by sorry
+-- (b^2 + 2bc + c^2) ≥ bc
+have hbc2 : b ^ 2 + c ^ 2 ≥ 2 * b * c := two_mul_le_add_sq b c
 
--- b^2 + c^2 ≥ 2bc
-have hbc : b ^ 2 + c ^ 2 ≥ 2 * b * c := by
-  calc
-    b ^ 2 + c ^ 2 = (b + c) ^ 2 - 2 * b * c := by ring
-    _ = 4 * ((b + c) / 2) ^ 2 - 2 * b * c := by ring
-    _ ≥ 4 * ((b * c) ^ (1/2)) ^ 2 - 2 * b * c := by rel [bc_am_gm]
-    _ = 4 * b * c - 2 * b * c := by sorry
-    _ = 2 * b * c := by ring
+-- (a^2 + 2ac + c^2) ≥ ac
+have hac2 : a ^ 2 + c ^ 2 ≥ 2 * a * c := two_mul_le_add_sq a c
 
--- apply AM-GM to a,c
-have ac_am_gm : (1/2) * a + (1/2) * c ≥ (a * c) ^ (1/2) := by sorry
-
--- a^2 + c^2 ≥ 2ac
-have hac : a ^ 2 + c ^ 2 ≥ 2 * a * c := by
-  calc
-    a ^ 2 + c ^ 2 = (a + c) ^ 2 - 2 * a * c := by ring
-    _ = 4 * ((a + c) / 2) ^ 2 - 2 * a * c := by ring
-    _ ≥ 4 * ((a * c) ^ (1/2)) ^ 2 - 2 * a * c := by rel [ab_am_gm]
-    _ = 4 * a * c - 2 * a * c := by sorry
-    _ = 2 * a * c := by ring
+-- the rest from previous ver
 
 -- add three cases of AM-GM and divide by 2
 -- (a^2 + b^2) + (b^2 + c^2) + (a^2 + c^2) ≥ 2ab + 2bc + 2ac
